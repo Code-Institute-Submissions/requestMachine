3d17499e7f07a738ec722783437c61ce6e10c1b8
asd <varroz56@gmail.com>
2019-12-28 22:17:39 +0000

    put back MONGO_URI as get app error

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'requestMachineDB'
-app.config['MONGO_URI'] = os.getenv('MONGO_URI', 'mongodb://localhost')
+app.config['MONGO_URI'] = 'mongodb+srv://dbAdmin:7sohc2fjtQp4kEgY@myfirstcluster-ji3yz.mongodb.net/requestMachineDB?retryWrites=true&w=majority'
 
 
 mongo = PyMongo(app)
